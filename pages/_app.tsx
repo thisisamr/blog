b2f1cd06b7d3ac3ed6488d57061f22bec23c2d2b
Amr Soliman <thisissoliman@protonmail.com>
2024-05-13 12:56:07 +0300

    added caveat font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,14 @@
 import Provider from "@/components/Provider";
 import "../globals.css";
 import type { AppProps } from "next/app";
-import { Roboto_Flex, Roboto_Mono } from "next/font/google";
+import { Roboto_Flex, Roboto_Mono, Caveat } from "next/font/google";
 
 const robotoFlex = Roboto_Flex({
   subsets: ["latin"],
 });
-
+const caveat = Caveat({
+  subsets: ["latin"],
+});
 const robotoMono = Roboto_Mono({
   subsets: ["latin"],
 });
@@ -14,18 +16,20 @@ const robotoMono = Roboto_Mono({
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-     <style jsx global>{`
+      <style jsx global>{`
         html {
           font-family: ${robotoFlex.style.fontFamily};
         }
-
+        .caveat-font {
+          font-family: ${caveat.style.fontFamily};
+        }
         .roboto-mono {
           font-family: ${robotoMono.style.fontFamily};
         }
       `}</style>
-    <Provider>
-      <Component {...pageProps} />;
-    </Provider>
+      <Provider>
+        <Component {...pageProps} />;
+      </Provider>
     </>
   );
 }
