376527740ba8c15f5ec9b7bf1ab98caed4fa8bf2
Amr Soliman <thisissoliman@protonmail.com>
2024-05-24 01:09:48 +0300

    change font to inter

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import Provider from "@/components/Provider";
 import "../globals.css";
 import type { AppProps } from "next/app";
-import { Roboto_Flex, Roboto_Mono, Caveat } from "next/font/google";
+import { Roboto_Flex, Roboto_Mono, Caveat,M_PLUS_Rounded_1c, Inter } from "next/font/google";
 
 const robotoFlex = Roboto_Flex({
   subsets: ["latin"],
@@ -12,19 +12,22 @@ const caveat = Caveat({
 const robotoMono = Roboto_Mono({
   subsets: ["latin"],
 });
-
+const inter =Inter({subsets:["latin"],weight:"400"});
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <style jsx global>{`
         html {
-          font-family: ${robotoFlex.style.fontFamily};
+          font-family: ${inter.style.fontFamily};
         }
         .caveat-font {
           font-family: ${caveat.style.fontFamily};
         }
         .roboto-mono {
           font-family: ${robotoMono.style.fontFamily};
+        }
+        .inter{
+          font-family:${inter.style.fontFamily}
         }
       `}</style>
       <Provider>
