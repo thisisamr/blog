de454ae04da4b2575fda41d98487a5f4d412964d
Amr Soliman <thisissoliman@protonmail.com>
2024-07-04 18:49:37 +0300

    change revalidate path url

--- pages/api/revalidate.ts
+++ pages/api/revalidate.ts
@@ -11,7 +11,7 @@ async function handler(req:NextApiRequest, res:NextApiResponse) {
     try {
       // this should be the actual path not a rewritten path
       // e.g. for "/blog/[slug]" this should be "/blog/post-1"
-      revalidatePath(`/blog/${slug}`)
+      revalidatePath('/blog/[slug]', 'page')
       return res.json({ revalidated: true })
     } catch (err) {
       // If there was an error, Next.js will continue
